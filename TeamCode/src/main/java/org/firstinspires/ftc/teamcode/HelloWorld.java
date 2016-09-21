d1bb4e2cbf0a17d372011ea6c99e9f8bc032e34f
acarroll <carroll3x@gmail.com>
2016-09-21 12:35:39 -0500

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+
+/**
+ * Created by acarroll on 9/21/16.
+ */
+public class HelloWorld {
+
+    public void printMsg() {
+
+        System.out.println("Hellow World");
+    }
+}
